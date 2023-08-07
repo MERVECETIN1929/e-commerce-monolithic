01a457018b740a39c90f766cf011a7f9480da8dd
Merve Cetin <mervecetinnitec@gmail.com>
2023-08-07 08:35:01 +0300

    delete file

--- src/main/java/com/example/ecommercemono2/common/security/MatchIdsService.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommercemono2.common.security;
-
-import jakarta.servlet.http.HttpServletRequest;
-
-import java.util.UUID;
-
-public interface MatchIdsService {
-    public boolean matchIds(HttpServletRequest request, UUID userId);
-}
